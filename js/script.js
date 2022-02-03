cf4817b7ad09b447e5f8cb7c69a2e03436f582b1
Henrique Correa de Lima <henriquehcl@hotmail.com>
2022-02-03 13:14:28 -0300

    adicionando novas telas, e ajustando layot

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 function status() {
+var situacao = document.getElementById('situacao')
+var situacao_texto = window.document.getElementById('situacao').innerText
+var data = window.document.getElementById('data').innerText
+var data_final = data.slice(13,23)
+var data_final = data_final.replace(' ','').replaceAll('/','')
+var data_final = parseInt(data_final)
+var data_atual_ = new Date();
+/* var mes = String(data_atual.getMonth() + 1).padStart(2, '0');
+var data_atual = parseInt(data_atual.getDate()+mes+data_atual.getFullYear())
 
-
+if (data_atual > data_final) {
+  situacao.classList.remove('situacao');
+  situacao.classList.add('atrasado');
+}else if (data_atual <= data_final) {
+  situacao.classList.remove('situacao');
+  situacao.classList.add('aberto');
+}*/
+//situacao.classList.remove('situacao');
+//situacao.classList.add('encerrado');
+tipo = typeof data_atual_
+console.log(tipo)
 
 }
